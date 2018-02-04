cbb6ca567ee6c8b51d77272bdac08d7f2236c8ad
unknown <sayanmaity.10@gmail.com>
2018-02-04 15:53:38 -0500

    Adding Data to repo7

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
-from flask import Flask, render_template
-import requests   
-import numpy as np
-from bokeh.plotting import figure, output_notebook, show
-from bokeh import embed
+from flask import Flask
 app = Flask(__name__)
-
 @app.route('/')
-def hello_world():
-	return '<h1>Bokeh plot embeded With Flask Webapp. Data fetched from QUANDL API; moving average convergence-divergence analysis is also shown in plot. </h1><a href=https://finance.yahoo.com/lookup?s=API>Go to plot page</a>'
-	
-
-if __name__ == '__main__':
-  app.run(port=33507)
+def index():
+	return 'Yo, it's working!'
+if __name__ == "__main__":
+	app.run()
